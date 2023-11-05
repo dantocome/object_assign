f2b143583c38b369d65edc10314da875311bc492
Daniel Egele <danielegele9@gmail.com>
2023-11-06 00:35:30 +0100

    all added files

---
+++ js/object.js
@@ -0,0 +1,46 @@
+
+let programming = {
+    languages: ["JavaScript", "Python", "Ruby"],
+    isChallenging: true,
+    isRewarding: true,
+    difficulty: 8,
+    jokes: "https://www.devtopics.com/best-programming-jokes/",
+
+};
+
+// Q1
+// languages.push("Go");
+// console.log(programming);
+
+// Q2
+//    if("difficulty" in programming){
+//     programming["difficulty"] = "7";
+
+//    }
+//    console.log(programming);
+
+// Q3
+    // delete programming.jokes;
+    // console.log(programming);
+
+ // Q4
+    // programming.isFun = "true";
+    // console.log(programming);
+
+//  Q5
+//    for(let objkey in languages){
+//     console.log(languages[objkey]);
+//    }
+
+//   Q6
+    // for(let objKey in programming){
+    //     console.log(programming[objKey]);
+
+    // }
+
+    // Q7
+    // for(let object.values in programming){
+    //    console.log(programming[object.values]); 
+    // }
+
+    // NB: NOT yet done...
